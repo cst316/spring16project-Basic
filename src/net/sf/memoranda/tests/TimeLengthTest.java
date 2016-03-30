a447f0207d0aaa8bfcb7e26e1db4869c066ad0e2
Aaron Lajom <alajom@asu.edu>
2016-03-29 22:21:40 -0700

    Setup a Unit Test for TimeLength and got CalendarDateTest to pass

---
+++ src/net/sf/memoranda/tests/TimeLengthTest.java
@@ -0,0 +1,47 @@
+package net.sf.memoranda.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import net.sf.memoranda.date.CalendarDate;
+
+public class TimeLengthTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void equalsTest() {
+		
+	}
+	
+	@Test
+	public void beforeTest() {
+		
+	}
+	
+	@Test
+	public void afterTest() {
+		
+	}
+
+}
