6a646d51a8de181ae0e8c589dc0074ad0ac418ce
Aaron Lajom <alajom@asu.edu>
2016-03-29 21:40:08 -0700

    I created a new class Time length to make it easier to hold times. I also edited the calcTime method to be usable as well as add methods for generating the string and adding progress and time to their respected vectors.

--- src/net/sf/memoranda/TimeProgRecord.java
+++ src/net/sf/memoranda/TimeProgRecord.java
@@ -9,8 +9,8 @@ public class TimeProgRecord {
 
 	/*We need to dynamically allocate the dates
 	 * or whatever data we decide needs to be put in these vectors*/
-    static int Vector _progress = new Vector();
-    static Vector _dates = new Vector();
+    static Vector<Integer> progress = new Vector<>();
+    static Vector<TimeLength> times = new Vector<>();
 
     /*
      * Following the style of EventsScheduler.java
@@ -24,9 +24,13 @@ public class TimeProgRecord {
 	 *currentTime will serve as the placeholder attribute for the current amount of time
 	 *prevTimeinMilli is the attribute that holds the previous amount of time
 	 **/
+	
 	int accumTime;
 	long prevTimeinMilli;
 	long currentTimeinMilli;
+	
+	//This is a placeholder for effort in case multiple clicks are used to increment effort
+	int progressCumulative;
 	
 	//The string that will be outputed to the Agenda Frame
 	String progString;
@@ -34,8 +38,9 @@ public class TimeProgRecord {
 	//Constructor
 	TimeProgRecord(TaskImpl task){
 		currentTimeinMilli = System.currentTimeMillis();
-		calcTime();
-		_progress.addElement(task.getProgress());
+		//calcTime();
+		//_progress.addElement(task.getProgress());
+		progressCumulative = 0;
 	}
 	
 	/*
@@ -43,40 +48,96 @@ public class TimeProgRecord {
 	 *add that time to accumTime,
 	 *then return that time in a string that can be appended to the progress string
 	 **/
-	public void calcTime(){
+	
+	/*
+	 * Aaron Lajom's change:
+	 * I converted this into a time converter that uses my class TimeLength.
+	 * The idea is to call this whenever you add progress values and to see if
+	 * the last change was done within minutes or less.
+	 */
+	public TimeLength calcTime(){
 		
-		private static final int SECOND = 1000;
-		private static final int MINUTE = 60 * SECOND;
-		private static final int HOUR = 60 * MINUTE;
-		private static final int DAY = 24 * HOUR;
+		String typeTime;
+		int SECOND = 1000;
+		int MINUTE = 60 * SECOND;
+		int HOUR = 60 * MINUTE;
+		int DAY = 24 * HOUR;
 		
-		progString.append("You have made " + task.getProgress() + " progress in, ");
+		//progString.append("You have made " + task.getProgress() + " progress in, ");
 		
 		//Give Previous time the former Current Time and set Current Time to the new SystemTime
-		prevTimeinMilli = currentTimeinMilli;
-		currentTimeinMilli = System.currentTimeMillis();
+		long prevTimeinMilli = currentTimeinMilli;
+		long currentTimeinMilli = System.currentTimeMillis();
 		
 		//Calculate how many Days Hours and Minutes are in the difference of dates
-		long milliDifference = currenTimeinMilli - prevTimeInMilli;
+		long milliDifference = currentTimeinMilli - prevTimeinMilli;
 
-		if (milliDifference > DAY) {
-		  progString.append(milliDifference / DAY).append(" days ");
+		if (milliDifference%DAY > 0) {
+		  //progString.append(milliDifference / DAY).append(" days ");
 		  milliDifference %= DAY;
+		  typeTime = "Days";
 		}
-		if (milliDifference > HOUR) {
-		  progString.append(milliDifference / HOUR).append(" hours ");
+		if (milliDifference%HOUR > 0) {
+		  //progString.append(milliDifference / HOUR).append(" hours ");
 		  milliDifference %= HOUR;
+		  typeTime = "Hours";
 		}
-		if (milliDifference > MINUTE) {
-		  progString.append(milliDifference / MINUTE).append(" minutes ");
+		if (milliDifference%MINUTE > 0) {
+		  //progString.append(milliDifference / MINUTE).append(" minutes ");
 		  milliDifference %= MINUTE;
+		  typeTime = "Minutes";
 		}
-		if (milliDifference > SECOND) {
-		  progString.append(milliDifference / SECOND).append(" seconds ");
+		else {
+		  //progString.append(milliDifference / SECOND).append(" seconds ");
 		  milliDifference %= SECOND;
+		  typeTime = "Seconds";
 		}
 		
 		//Add the difference to the accumulated total
-		acuumTime += milliDifference;
+		//acuumTime += milliDifference;
+		
+		TimeLength myTime = new TimeLength(typeTime, milliDifference);
+		return myTime;
 	}
+	
+	/*
+	 * Aaron Lajom
+	 * The purpose of this method is to append both time and progress to the their respected vectors
+	 */
+	public void appendProgAndTime(int myProg) {
+		
+		//Calculate the current difference in time
+		TimeLength thisTime = calcTime ();
+		
+		int progSize = progress.size();
+		int progMaxRef = progSize - 1;
+		int timesSize = times.size();
+		//int timesMaxRef = timesSize - 1;
+		
+		//If the size of the times vector is not zero and barely any time has passed, it will add the progress 
+		//to the last bit of progress
+		if (timesSize != 0 && (thisTime.getType().equals("Minutes") || thisTime.getType().equals("Seconds"))) {
+			progress.set(progMaxRef, progress.get(progMaxRef) + myProg);
+		}
+		//Otherwise just add it
+		else { 
+			times.add(thisTime);
+			progress.add(myProg);
+		}
+		
+	}
+	
+	public void generateString () {
+		if (times.size() == 0) {
+			progString = "No progress has been made!";
+		}
+		else {
+			progString = "Progress: ";
+			
+			for(int i = 0; i < progress.size(); i++) {
+				progString += (times.get(i).getLength() + " " + times.get(i).getType() + ", ");
+			}
+		}
+	}
+
 }
